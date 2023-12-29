c2b388992b72b25ec02dfd73ebad6085ee295d02
Diego Barreiro Perez <diegobp@amazon.com>
2023-12-29 23:34:16 +0100

    Remove Pydantic Dependency

--- json_logic_asp/models/clingo_dto.py
+++ json_logic_asp/models/clingo_dto.py
@@ -1,9 +1,11 @@
-from typing import List
-
-from pydantic import BaseModel, Field
+from typing import List, Optional
 
 
-class ClingoOutput(BaseModel):
-    success: bool
-    has_mapping: bool = Field(default=False)
-    matching_rules: List[str] = Field(default_factory=list)
+class ClingoOutput:
+    def __init__(self, success: bool, has_mapping: bool = False, matching_rules: Optional[List[str]] = None):
+        if matching_rules is None:
+            matching_rules = []
+
+        self.success: bool = success
+        self.has_mapping: bool = has_mapping
+        self.matching_rules: List[str] = matching_rules
---
+++ tests/test_models/test_translator_dto.py
@@ -0,0 +1,55 @@
+from json_logic_asp.models.translator_dto import RuleInput, DataInput, RuleOutput, DataOutput
+
+from tests.fixtures import cuid_fixture  # noqa
+
+
+def test_rule_input():
+    rule_input = RuleInput(
+        rule_tree={"a": {"b": "c"}}
+    )
+    assert rule_input.rule_tree == {"a": {"b": "c"}}
+    assert rule_input.rule_id == "mock1"
+
+
+def test_rule_input_with_custom_id():
+    rule_input = RuleInput(
+        rule_tree={"a": {"b": "c"}},
+        rule_id="custom",
+    )
+    assert rule_input.rule_tree == {"a": {"b": "c"}}
+    assert rule_input.rule_id == "custom"
+
+
+def test_rule_output():
+    output = RuleOutput(
+        statements=["a", "b"],
+        rule_mapping={"a": "c", "b": "d"}
+    )
+    assert output.statements == ["a", "b"]
+    assert output.rule_mapping == {"a": "c", "b": "d"}
+
+
+def test_data_input():
+    data_input = DataInput(
+        data_object={"a": {"b": "c"}}
+    )
+    assert data_input.data_object == {"a": {"b": "c"}}
+    assert data_input.data_id == "mock1"
+
+
+def test_data_input_with_custom_id():
+    data_input = DataInput(
+        data_object={"a": {"b": "c"}},
+        data_id="custom",
+    )
+    assert data_input.data_object == {"a": {"b": "c"}}
+    assert data_input.data_id == "custom"
+
+
+def test_data_output():
+    output = DataOutput(
+        statements=["a", "b"],
+        data_mapping={"a": "c", "b": "d"}
+    )
+    assert output.statements == ["a", "b"]
+    assert output.data_mapping == {"a": "c", "b": "d"}
