8a3b6203f01df8473fd2b3f9cf1dc6010f835252
Diego Barreiro Perez <diegobp@amazon.com>
2024-01-01 13:24:45 +0100

    chore: Fix formatting

--- tests/test_translator/test_rule_generator.py
+++ tests/test_translator/test_rule_generator.py
@@ -68,19 +68,19 @@ def test_is_valid_json_logic_node_custom_op():
         ({"missing": "a"}, DataMissingNode("a")),
         ({"==": [{"var": "a"}, "b"]}, LogicEqualNode(DataVarNode("a"), "b")),
         (
-                {
-                    "and": [
-                        {"==": [{"var": "a"}, "b"]},
-                        {"missing": "c"},
-                    ]
-                },
-                BooleanAndNode(LogicEqualNode(DataVarNode("a"), "b"), DataMissingNode("c")),
+            {
+                "and": [
+                    {"==": [{"var": "a"}, "b"]},
+                    {"missing": "c"},
+                ]
+            },
+            BooleanAndNode(LogicEqualNode(DataVarNode("a"), "b"), DataMissingNode("c")),
         ),
         (
-                {
-                    "!": {"var": "a"},
-                },
-                BooleanNotNode(DataVarNode("a")),
+            {
+                "!": {"var": "a"},
+            },
+            BooleanNotNode(DataVarNode("a")),
         ),
     ],
     ids=[
@@ -112,117 +112,111 @@ def test_parse_json_logic_node_custom_op():
     "rule_inputs, with_comments, custom_nodes, expected_statements, expected_mapping",
     [
         (
-                [
-                    RuleInput(
-                        rule_id="test",
-                        rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
-                    ),
-                ],
-                False,
-                None,
-                [
-                    "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
-                    "and(mock3) :- eq(mock2).",
-                    "rule(s098f6bcd4621d373cade4e832627b4f6) :- and(mock3).",
-                ],
-                {
-                    "s098f6bcd4621d373cade4e832627b4f6": "test"
-                },
+            [
+                RuleInput(
+                    rule_id="test",
+                    rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
+                ),
+            ],
+            False,
+            None,
+            [
+                "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
+                "and(mock3) :- eq(mock2).",
+                "rule(s098f6bcd4621d373cade4e832627b4f6) :- and(mock3).",
+            ],
+            {"s098f6bcd4621d373cade4e832627b4f6": "test"},
         ),
         (
-                [
-                    RuleInput(
-                        rule_id="test",
-                        rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
-                    ),
-                ],
-                True,
-                None,
-                [
-                    "% a EQ b",
-                    "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
-                    "and(mock3) :- eq(mock2).",
-                    "% test",
-                    "rule(s098f6bcd4621d373cade4e832627b4f6) :- and(mock3).",
-                ],
-                {
-                    "s098f6bcd4621d373cade4e832627b4f6": "test"
-                },
+            [
+                RuleInput(
+                    rule_id="test",
+                    rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
+                ),
+            ],
+            True,
+            None,
+            [
+                "% a EQ b",
+                "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
+                "and(mock3) :- eq(mock2).",
+                "% test",
+                "rule(s098f6bcd4621d373cade4e832627b4f6) :- and(mock3).",
+            ],
+            {"s098f6bcd4621d373cade4e832627b4f6": "test"},
         ),
         (
-                [
-                    RuleInput(
-                        rule_id="test1",
-                        rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
-                    ),
-                    RuleInput(
-                        rule_id="test2",
-                        rule_tree={"or": {"<": [{"var": "c"}, "d"]}},
-                    ),
-                ],
-                True,
-                None,
-                [
-                    "% a EQ b",
-                    "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
-                    "and(mock3) :- eq(mock2).",
-                    "% c LT d",
-                    "lt(mock5) :- var(s4a8a08f09d37b73795649038408b5f33, V1), V1 < s8277e0910d750195b448797616e091ad.",
-                    "or(mock6) :- lt(mock5).",
-                    "% test1",
-                    "rule(s5a105e8b9d40e1329780d62ea2265d8a) :- and(mock3).",
-                    "% test2",
-                    "rule(sad0234829205b9033196ba818f7a872b) :- or(mock6).",
-                ],
-                {
-                    "s5a105e8b9d40e1329780d62ea2265d8a": "test1",
-                    "sad0234829205b9033196ba818f7a872b": "test2",
-                },
+            [
+                RuleInput(
+                    rule_id="test1",
+                    rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
+                ),
+                RuleInput(
+                    rule_id="test2",
+                    rule_tree={"or": {"<": [{"var": "c"}, "d"]}},
+                ),
+            ],
+            True,
+            None,
+            [
+                "% a EQ b",
+                "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
+                "and(mock3) :- eq(mock2).",
+                "% c LT d",
+                "lt(mock5) :- var(s4a8a08f09d37b73795649038408b5f33, V1), V1 < s8277e0910d750195b448797616e091ad.",
+                "or(mock6) :- lt(mock5).",
+                "% test1",
+                "rule(s5a105e8b9d40e1329780d62ea2265d8a) :- and(mock3).",
+                "% test2",
+                "rule(sad0234829205b9033196ba818f7a872b) :- or(mock6).",
+            ],
+            {
+                "s5a105e8b9d40e1329780d62ea2265d8a": "test1",
+                "sad0234829205b9033196ba818f7a872b": "test2",
+            },
         ),
         (
-                [
-                    RuleInput(
-                        rule_id="test1",
-                        rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
-                    ),
-                    RuleInput(
-                        rule_id="test2",
-                        rule_tree={"or": {"==": [{"var": "a"}, "b"]}},
-                    ),
-                ],
-                True,
-                None,
-                [
-                    "% a EQ b",
-                    "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
-                    "and(mock3) :- eq(mock2).",
-                    "or(mock6) :- eq(mock2).",
-                    "% test1",
-                    "rule(s5a105e8b9d40e1329780d62ea2265d8a) :- and(mock3).",
-                    "% test2",
-                    "rule(sad0234829205b9033196ba818f7a872b) :- or(mock6).",
-                ],
-                {
-                    "s5a105e8b9d40e1329780d62ea2265d8a": "test1",
-                    "sad0234829205b9033196ba818f7a872b": "test2",
-                },
+            [
+                RuleInput(
+                    rule_id="test1",
+                    rule_tree={"and": {"==": [{"var": "a"}, "b"]}},
+                ),
+                RuleInput(
+                    rule_id="test2",
+                    rule_tree={"or": {"==": [{"var": "a"}, "b"]}},
+                ),
+            ],
+            True,
+            None,
+            [
+                "% a EQ b",
+                "eq(mock2) :- var(s0cc175b9c0f1b6a831c399e269772661, V1), V1 == s92eb5ffee6ae2fec3ad71c777531578f.",
+                "and(mock3) :- eq(mock2).",
+                "or(mock6) :- eq(mock2).",
+                "% test1",
+                "rule(s5a105e8b9d40e1329780d62ea2265d8a) :- and(mock3).",
+                "% test2",
+                "rule(sad0234829205b9033196ba818f7a872b) :- or(mock6).",
+            ],
+            {
+                "s5a105e8b9d40e1329780d62ea2265d8a": "test1",
+                "sad0234829205b9033196ba818f7a872b": "test2",
+            },
         ),
         (
-                [
-                    RuleInput(
-                        rule_id="test",
-                        rule_tree={"op": {"dummy"}},
-                    ),
-                ],
-                True,
-                {"op": DummyTestNode},
-                [
-                    "% test",
-                    "rule(s098f6bcd4621d373cade4e832627b4f6) :- dummy(mock1).",
-                ],
-                {
-                    "s098f6bcd4621d373cade4e832627b4f6": "test"
-                },
+            [
+                RuleInput(
+                    rule_id="test",
+                    rule_tree={"op": {"dummy"}},
+                ),
+            ],
+            True,
+            {"op": DummyTestNode},
+            [
+                "% test",
+                "rule(s098f6bcd4621d373cade4e832627b4f6) :- dummy(mock1).",
+            ],
+            {"s098f6bcd4621d373cade4e832627b4f6": "test"},
         ),
     ],
     ids=[
@@ -231,10 +225,11 @@ def test_parse_json_logic_node_custom_op():
         "multi_rule",
         "node_cache",
         "custom_nodes",
-    ]
+    ],
 )
-def test_generate_multiple_rule_asp_definition(rule_inputs, with_comments, custom_nodes,
-                                               expected_statements, expected_mapping):
+def test_generate_multiple_rule_asp_definition(
+    rule_inputs, with_comments, custom_nodes, expected_statements, expected_mapping
+):
     statements, mapping = generate_multiple_rule_asp_definition(
         rule_inputs=rule_inputs,
         with_comments=with_comments,
@@ -258,9 +253,9 @@ def test_generate_multiple_rule_asp_definition(rule_inputs, with_comments, custo
         "with_comments",
         "custom_nodes",
         "both",
-    ]
+    ],
 )
-@patch('json_logic_asp.translator.rule_generator.generate_multiple_rule_asp_definition')
+@patch("json_logic_asp.translator.rule_generator.generate_multiple_rule_asp_definition")
 def test_generate_single_rule_asp_definition(mock_generate_multiple_rule_asp_definition, with_comments, custom_nodes):
     mock_generate_multiple_rule_asp_definition.return_value = ("stmt1\nstmt2", {"a": "b"})
 
@@ -272,5 +267,7 @@ def test_generate_single_rule_asp_definition(mock_generate_multiple_rule_asp_def
 
     assert asp_definition == "stmt1\nstmt2"
     mock_generate_multiple_rule_asp_definition.assert_called_once_with(
-        rule_inputs=[ri], with_comments=with_comments, custom_nodes=custom_nodes,
+        rule_inputs=[ri],
+        with_comments=with_comments,
+        custom_nodes=custom_nodes,
     )
